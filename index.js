b2b09b349e996422d62855f9e89caa454a7f22e3
chris <chris@github.com>
2019-08-16 22:48:21 +1000

    config docker-compose and add redis

--- index.js
+++ index.js
@@ -2,29 +2,41 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const account = require("./routers/userRoute");
-const test = require("./routers/testRoute");
+const redis = require("redis");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/account", account);
-app.use("/test", test);
 
-app.get("/aaa/", (req, res) => {
-  res.status(200).send("<h1>test</h1>");
+app.get("/health-check", (req, res) => {
+  return res.status(200).send("healthy...");
 });
 
 app.get("/api-auth", (req, res) => {
   res.status(200).send("<h1>test</h1>");
 });
 
+const redis_host = process.env.REDIS_HOST || "localhost";
+const redis_port = process.env.REDIS_PORT || 6379;
+const redisClient = redis.createClient({
+  host: redis_host,
+  port: redis_port
+});
+
+redisClient.set("string key", "string value", redis.print);
+
+const mongo_host = process.env.MONGO_HOST || "localhost";
+const mongo_port = process.env.MONGO_PORT || 27017;
+const mongo_database = process.env.MONGO_DB || "Chris";
+
 mongoose
-  .connect("mongodb://localhost/CASDash", {
+  .connect(`mongodb://${mongo_host}:${mongo_port}/${mongo_database}`, {
     useNewUrlParser: true, //removing warning
     useCreateIndex: true //removing warning
   })
   .then(() => console.log("connect to db..."))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening to port:${port}...`));
--- routers/testRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const Route = express.Router();
-
-Route.get("/api-auth", (req, res) => {
-  res.status(200).send("<h1>hello world</h1>");
-});
-
-module.exports = Route;
